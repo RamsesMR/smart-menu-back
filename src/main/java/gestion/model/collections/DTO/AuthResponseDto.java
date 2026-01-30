613a11e75e74f160b551b7420f512ce00c0b2ac3
BryanStrk <bryan_ed_8@hotmail.com>
2026-01-30 01:32:19 +0100

    JWT integrado + filtro + securityconfig

---
+++ src/main/java/gestion/model/collections/DTO/AuthResponseDto.java
@@ -0,0 +1,13 @@
+package gestion.model.collections.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class AuthResponseDto {
+  private String token;
+  private UsuarioDto user;
+}
