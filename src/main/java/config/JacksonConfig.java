9c95b5b5da395731c586afcc6cab9e3435353199
BryanStrk <bryan_ed_8@hotmail.com>
2026-03-12 13:45:59 +0100

    refactor: replace deprecated Jackson2ObjectMapperBuilder with JsonMapper.builder()

--- src/main/java/config/JacksonConfig.java
+++ src/main/java/config/JacksonConfig.java
@@ -3,26 +3,20 @@ package config;
 import org.bson.types.ObjectId;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-
-import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
 @Configuration
 public class JacksonConfig {
 
-    @Bean
-    public Module objectIdModule() {
-        SimpleModule module = new SimpleModule();
-        module.addSerializer(ObjectId.class, ToStringSerializer.instance);
-        return module;
-    }
-
-    @Bean
-    public Jackson2ObjectMapperBuilder jacksonBuilder(Module objectIdModule) {
-        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-        builder.modules(objectIdModule);
-        return builder;
-    }
+	@Bean
+	ObjectMapper objectMapper() {  // sin 'public'
+	    SimpleModule module = new SimpleModule();
+	    module.addSerializer(ObjectId.class, ToStringSerializer.instance);
+	    return JsonMapper.builder()
+	            .addModule(module)
+	            .build();
+	}
 }
